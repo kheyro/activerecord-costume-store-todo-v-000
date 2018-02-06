87eaf73e40bbe3fd4a1c4183f7f24aec2da99f55
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 22:03:56 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration
+  def change
+    create_table :HauntedHouse do |t|
+      t.string :name,
+      t.text :location,
+      t.string :theme,
+      t.float :price,
+      t.boolean :family_friendly,
+      t.string :opening_date,
+      t.string :closing_date,
+      t.text :description
+    end
+  end
+end
